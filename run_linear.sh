570c1f186b19c2c145fd4eec0812751bb464927d
PinkTheDemon <1929833884@qq.com>
2024-09-18 15:48:13 +0800

    shell脚本自动测试参数

---
+++ run_linear.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 定义参数列表
+cov_list=(
+    "1e-4"
+    "1e-2"
+    "1e0"
+    "1e2"
+    "1e4"
+)
+goodInit_list=(
+    # ""
+    "True"
+)
+gamma_list=(
+    0.4
+    0.6
+    0.8
+    0.99
+    1.0
+)
+
+# 循环执行Python脚本
+for goodInit in "${goodInit_list[@]}"
+do
+    for gamma in "${gamma_list[@]}"
+    do 
+        for cov in "${cov_list[@]}"
+        do
+            python ./linear.py --cov $cov --goodInit $goodInit --gamma $gamma
+        done
+    done
+done
